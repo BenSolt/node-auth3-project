a15a38896384b194d70ab1be0dfbd2691e6300a4
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-29 14:50:18 -0500

    need to do migrate latest, knex seed make

--- database/migrations/20200129143638_users-table.js
+++ database/migrations/20200129143638_users-table.js
@@ -7,6 +7,8 @@ exports.up = function(knex) {
             .unique();
         tbl.string('password', 128)
             .notNullable();
+        tbl.string('department', 128)
+            .notNullable();
     });
   
 };
